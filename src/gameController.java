b6d5314bf30e1dcc10412ce2c0986c8f51ab5ad2
Magnus <magnusrk90@gmail.com>
2022-10-17 11:39:31 +0200

    Added methods to gameController

--- src/gameController.java
+++ src/gameController.java
@@ -2,4 +2,14 @@ class gameController {
     public gameController(){
 
     }
+    public void play(){
+
+    }
+    public void callPlayer(int player){
+
+    }
+
+    public void resetGame(){
+
+    }
 }
