9a7b49a43cc6c1d2642360156229d3e1d5826c4b
Magnus <magnusrk90@gmail.com>
2022-10-17 12:27:24 +0200

    Game now ask for which langauge the player wants

--- src/gameController.java
+++ src/gameController.java
@@ -1,23 +1,56 @@
+import java.util.Scanner;
+
 class gameController {
+
+    private String language;
     public gameController(){
 
     }
     public void play(){
-
+        setLang();
     }
-    public void setLang(String lang){
+    private void setLang(){
+        Language.SetLanguage("preLangPick");
+        System.out.println(Language.GetString("press"));
+        System.out.println(Language.GetString("english"));
+        System.out.println(Language.GetString("danish"));
+        System.out.println(Language.GetString("japanese"));
+        Scanner scan = new Scanner(System.in);
+        String lang = scan.next();
         switch (lang){
-            case "den" -> Language.SetLanguage("da");
-            case "eng" -> Language.SetLanguage("en");
-            case "jap" -> Language.SetLanguage("jp");
+            case "a":
+                Language.SetLanguage("en");
+                language = Language.GetString("getEng");
+                System.out.println(Language.GetString("languageSetText"));
+                break;
+            case "b":
+                Language.SetLanguage("da");
+                language = Language.GetString("getDan");
+                System.out.println(Language.GetString("languageSetText"));
+                break;
+            case "c":
+                Language.SetLanguage("jp");
+                language = Language.GetString("getJap");
+                System.out.println(Language.GetString("languageSetText"));
+                break;
+            default:
+                System.out.println(Language.GetString("tryAgain"));
+                System.out.println();
+                setLang();
         }
-        System.out.println(Language.GetString("languageSetText"));
     }
-    public void callPlayer(int player){
+    private void callPlayer(int player){
 
     }
 
     public void resetGame(){
 
     }
+
+
+    // Setters and getters
+    public String getLang() {
+        return language;
+    }
+
 }
