8aca25bdd461fa3f3607ec55a277d780cab9ccc5
Chayanee <s210973@dtu.dk>
2022-10-18 14:04:36 +0200

    Created die class

---
+++ src/die.java
@@ -0,0 +1,17 @@
+public class die {
+    private static final int DIESIDE = 6;
+
+    public die (){
+
+    }
+
+    public static int[] throwDice(){
+        int die1 = (int) ((Math.random() * (DIESIDE)) + 1);
+        int die2 = (int) ((Math.random() * (DIESIDE)) + 1);
+        return new int[]{die1, die2};
+    }
+
+    public static int getDIESIDE() {
+        return DIESIDE;
+    }
+}
