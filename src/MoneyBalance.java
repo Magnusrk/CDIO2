a9571ddbe0baa8918d04a24fc71edd972ee5b07e
Magnus <magnusrk90@gmail.com>
2022-10-21 13:19:06 +0200

    MoneyBalance minimum balance is now zero

--- src/MoneyBalance.java
+++ src/MoneyBalance.java
@@ -8,6 +8,9 @@ public class MoneyBalance {
     //Adds money to the players balance. x will be a variable from the field class
     public void addmoney(int addCash){
         balance+=addCash;
+        if (balance<0){
+            balance=0;
+        }
     }
 
     public int getBalance(){
