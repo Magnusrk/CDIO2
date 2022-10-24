fa0b162466245e4c112e8e334e8cedbc3b5afbc2
Magnus <magnusrk90@gmail.com>
2022-10-24 15:26:30 +0200

    Added field test

--- src/Field.java
+++ src/Field.java
@@ -5,8 +5,11 @@ public class Field {
     int addCash;
     /*Switch case to choose which field is landed on.
      It chooses based on the dice throw and outputs the corresponding field.*/
-    public void fieldGet(int dieCount) {
+    public int fieldGet(int dieCount) {
         if (dieCount >12) {
+            if (dieCount % 12 == 0){
+                dieCount = 2;
+            }
             dieCount = (dieCount % 12)+1;
         }
         switch (dieCount){
@@ -14,58 +17,59 @@ public class Field {
                 field = "Tower";
                 addCash = 250;
                 fieldTxt = Language.GetString("tower") + " " + Language.GetString("gain") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 2;
             case 3:
                 field = "Crater";
                 addCash = -100;
                 fieldTxt = Language.GetString("crater")+ " " + Language.GetString("lose") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 3;
             case 4:
                 field = "Palace gates";
                 addCash = 100;
                 fieldTxt = Language.GetString("palace")+ " " + Language.GetString("gain")+ " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 4;
             case 5:
                 field = "Cold Desert";
                 addCash = -20;
                 fieldTxt = Language.GetString("desert")+ " " + Language.GetString("lose") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 5;
             case 6:
                 field = "Walled city";
                 addCash = 180;
                 fieldTxt = Language.GetString("city") + " " + Language.GetString("gain" ) + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 6;
             case 7:
                 field = "Monastery";
                 addCash = 0;
                 fieldTxt = Language.GetString("monastery");
-                break;
+                return 7;
             case 8:
                 field = "Black cave";
                 addCash = -70;
                 fieldTxt = Language.GetString("cave") + " " + Language.GetString("lose") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 8;
             case 9:
                 field = "Huts in the mountain";
                 addCash = 60;
                 fieldTxt = Language.GetString("huts") + " " + Language.GetString("gain") + " " + Math.abs(addCash)+ Language.GetString("gold");
-                break;
+                return 9;
             case 10:
                 field = "The Werewall";
                 addCash = -80;
                 fieldTxt =  Language.GetString("werewall") + " " + Language.GetString("lose") + " " + Math.abs(addCash)+ Language.GetString("gold");
-                break;
+                return 10;
             case 11:
                 field = "The pit";
                 addCash = -50;
                 fieldTxt = Language.GetString("pit") + " " + Language.GetString("lose") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 11;
             case 12:
                 field = "Goldmine";
                 addCash = 650;
                 fieldTxt = Language.GetString("goldmine")+ " " + Language.GetString("gain") + " " + Math.abs(addCash) + Language.GetString("gold");
-                break;
+                return 12;
         }
+        return 0;
     }
 
 }
---
+++ src/FieldTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+    Field field = new Field();
+    @Test
+    void testFieldGet() {
+        for (int i = 2; i < 1000; i++) {
+            System.out.println(i);
+            assertTrue(field.fieldGet(i)<=12);
+            assertTrue(field.fieldGet(i)>=2);
+        }
+    }
+}
